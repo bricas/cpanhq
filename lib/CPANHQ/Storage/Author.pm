67961db6f0e1e132cc968c5356f3c0cbc89fb004
Brian Cassidy <bricas@cpan.org>
2009-04-27 15:23:53 -0300

    add basic Author table

---
+++ lib/CPANHQ/Storage/Author.pm
@@ -0,0 +1,41 @@
+package CPANHQ::Storage::Author;
+
+use strict;
+use warnings;
+
+use base qw( DBIx::Class );
+
+__PACKAGE__->load_components( qw( Core ) );
+__PACKAGE__->table( 'author' );
+__PACKAGE__->add_columns(
+    id => {
+        data_type         => 'bigint',
+        is_auto_increment => 1,
+        is_nullable       => 0,
+    },
+    cpanid => {
+        data_type   => 'varchar',
+        size        => 16,
+        is_nullable => 0,
+    },
+    name => {
+        data_type   => 'varchar',
+        size        => 255,
+        is_nullable => 0,
+    },
+    email => {
+        data_type   => 'varchar',
+        size        => 255,
+        is_nullable => 0,
+    },
+);
+__PACKAGE__->set_primary_key( qw( id ) );
+__PACKAGE__->resultset_attributes( { order_by => [ 'cpanid' ] } );
+__PACKAGE__->add_unique_constraint( [ 'cpanid' ] );
+
+sub display_name {
+    my $self = shift;
+    return sprintf '%s <%s>', $self->name, $self->email;
+}
+
+1;
