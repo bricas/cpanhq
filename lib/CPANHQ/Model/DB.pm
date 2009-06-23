26f1fe5016120825b64cfa0b948b8b726915ba96
Shlomi Fish <shlomif@iglu.org.il>
2009-06-23 17:56:36 +0300

    Added POD to lib/CPANHQ/Model/DB.pm .

--- lib/CPANHQ/Model/DB.pm
+++ lib/CPANHQ/Model/DB.pm
@@ -10,3 +10,38 @@ __PACKAGE__->config(
 );
 
 1;
+
+__END__
+
+=head1 NAME
+
+CPANHQ::Model::DB - the database model of CPANHQ
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is the database model class of CPANHQ. It uses L<CPANHQ::Storage> as
+its model.
+
+=head1 METHODS
+
+None.
+
+=head1 SEE ALSO
+
+L<CPANHQ>, L<Catalyst::Model::DBIC::Schema>, L<Catalyst>
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
