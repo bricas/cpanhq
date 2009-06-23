a7e24ca9194073b56ecc8a8ef40a1140e8876693
Shlomi Fish <shlomif@iglu.org.il>
2009-06-23 18:27:09 +0300

    Added POD to lib/CPANHQ/Form/Login.pm .

--- lib/CPANHQ/Form/Login.pm
+++ lib/CPANHQ/Form/Login.pm
@@ -3,7 +3,29 @@ package CPANHQ::Form::Login;
 use strict;
 use warnings;
 
+=head1 NAME
+
+CPANHQ::Form::Login - the CPANHQ login form.
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is the L<CPANHQ> login form.
+
+=head1 METHODS
+
+=cut
+
 use parent qw( CPANHQ::Base::Form );
+
+=head2 $self->build_form()
+
+Builds the form.
+
+=cut
 
 sub build_form {
     my $self = shift;
@@ -15,3 +37,19 @@ sub build_form {
 }
 
 1;
+
+=head1 SEE ALSO
+
+L<CPANHQ>, L<CPANHQ::Base::Form> .
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
