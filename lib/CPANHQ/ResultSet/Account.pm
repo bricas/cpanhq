2e551700589acd2a078a7f58b67919d391bf68a7
Shlomi Fish <shlomif@iglu.org.il>
2009-06-23 18:11:54 +0300

    Added POD.

--- lib/CPANHQ/ResultSet/Account.pm
+++ lib/CPANHQ/ResultSet/Account.pm
@@ -11,3 +11,39 @@ sub auto_create {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+CPANHQ::ResultSet::Account - an account result-set.
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is L<CPANHQ>'s account result-set.
+
+=head1 METHODS
+
+=head2 $self->auto_create( { openid => $openid })
+
+Creates an account automatically based on the $openid OpenID credentials.
+
+=head1 SEE ALSO
+
+L<CPANHQ>, L<DBIx::Class::ResultSet>
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
