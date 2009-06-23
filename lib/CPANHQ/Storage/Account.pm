9772069b34330f2c8a5ba023e4bcfbec8060060a
Shlomi Fish <shlomif@iglu.org.il>
2009-06-23 18:54:38 +0300

    Added POD to lib/CPANHQ/Storage/Account.pm .

--- lib/CPANHQ/Storage/Account.pm
+++ lib/CPANHQ/Storage/Account.pm
@@ -3,6 +3,25 @@ package CPANHQ::Storage::Account;
 use strict;
 use warnings;
 
+
+=head1 NAME
+
+CPANHQ::Storage::Account - a class representing a CPANHQ account
+
+=head1 SYNOPSIS
+      
+    my $schema = CPANHQ->model("DB");
+
+    my $accounts_rs = $schema->resultset('Account');
+
+=head1 DESCRIPTION
+
+This is the account schema class for L<CPANHQ>.
+
+=head1 METHODS
+
+=cut
+
 use base qw( DBIx::Class );
 
 __PACKAGE__->load_components( qw( TimeStamp Core ) );
@@ -40,9 +59,35 @@ __PACKAGE__->set_primary_key( 'id' );
 
 __PACKAGE__->add_unique_constraint( [ 'openid' ] );
 
+=head2 $self->display_name()
+
+Returns a display name for the account.
+
+=cut
+
 sub display_name {
     my $self = shift;
     return $self->username || $self->openid;
 }
+
+
+=head1 SEE ALSO
+
+L<CPANHQ::Storage>, L<CPANHQ>, L<DBIx::Class>
+
+=head1 AUTHOR
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+Shlomi Fish L<http://www.shlomifish.org/> (who places all his contributions
+and modifications under the public domain - 
+L<http://creativecommons.org/license/zero> )
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
